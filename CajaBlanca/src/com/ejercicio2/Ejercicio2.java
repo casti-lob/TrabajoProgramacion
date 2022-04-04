5ff31dc5c1d426833e2451da5b590a5f2bcbab54
casti-lob <casti_lob@hotmail.com>
2022-04-04 08:59:45 +0200

    Caja blanca terminado

--- CajaBlanca/src/com/ejercicio2/Ejercicio2.java
+++ CajaBlanca/src/com/ejercicio2/Ejercicio2.java
@@ -7,17 +7,17 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		
 		int[] vacio= new int[]{};// El flujo del programa no entra ninguna vez
-		int[] unaVez= new int[]{1}; //Pasa una única vez por el bucle
-		int[] dosVeces= new int[]{2,1};// Pasa dos veces por el bucle
-		int[] normal= new int[]{2,1,8,7,3,11,5,9,4,80 ,-8,-1};//Pasa m veces por el bucle donde m <n
+		int[] unaVez= new int[]{1}; //Pasa una ï¿½nica vez por el bucle
+		int[] dosVeces= new int[]{2,1};// Pasa dos veces por el bucle Tiene que estar ordenado
+		int[] normal= new int[]{2,1,8,7,3,11,5,9,4,80 ,-8,-1};//Pasa m veces por el bucle donde m <n tiene que estar ordenado y desordenado elementos
 		
-		System.out.println(ordenar(normal));
+		System.out.println(ordenar(unaVez));
 	}
 	
 	public static String ordenar (int[] numeros) {
 		String lista="";
 		int menor ,pos, tmp;
-		for(int i=0;i<numeros.length-1;i++) { //aplicamos un hit count superior al tamaño del array
+		for(int i=0;i<numeros.length-1;i++) { //aplicamos un hit count superior al tamaï¿½o del array Tiene que estar todo desordenado
 			menor=numeros[i];
 			pos=i;
 			for(int j=i+1;j<numeros.length;j++) {
