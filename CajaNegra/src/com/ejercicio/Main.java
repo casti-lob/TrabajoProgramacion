4774d420ce9c3e24bd48e95b3d7e97121d6a64e8
casti-lob <casti_lob@hotmail.com>
2022-04-05 16:28:47 +0200

    terminado caja negra

--- CajaNegra/src/com/ejercicio/Main.java
+++ CajaNegra/src/com/ejercicio/Main.java
@@ -2,17 +2,27 @@ package com.ejercicio;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
-	public static List<Empleado> empleados =new ArrayList<Empleado>();
-	public static Array [] registros =new arra
+	static Set<Registro> registros=new  HashSet<Registro>();
+	
 	public static void main(String[] args) {
+		
 		try {
 			Empleado e = new Empleado(001, "1234567891",12, '-');
-			System.out.println(e);
+			introducirRegistro(e);
+			System.out.println(registros);
+			
 		} catch (empleadoException e) {
 			System.out.println(e.getMessage());		}
 	}
-
+	public static void introducirRegistro(Empleado e) {
+		Registro r= new Registro();
+		r.crearRegistro(e);
+		registros.add(r);
+	}
+	
 }
--- CajaNegra/src/com/ejercicio/Registro.java
+++ CajaNegra/src/com/ejercicio/Registro.java
@@ -1,8 +1,64 @@
 package com.ejercicio;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class Registro {
+	private int nRegistro=0;
+	private static int acumulador=1;
+	private String prima;
+	private int numEmpleado;
+	
+	public Registro() {
+		super();
+		this.nRegistro = acumulador++;
+		this.numEmpleado=0;
+		this.prima=null;
+	}
+
+	public int getnRegistro() {
+		return nRegistro;
+	}
+
+	public int getNumEmpleado() {
+		return numEmpleado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nRegistro);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Registro other = (Registro) obj;
+		return nRegistro == other.nRegistro;
+	}
+	
+	
+	public  void crearRegistro(Empleado e) {
+		if(e.getMesesTrabajo()>=12&& e.getDirectivo()=='+') {
+			this.prima="P1";
+			
+		}else if(e.getMesesTrabajo()>=12&& e.getDirectivo()=='-') {
+			this.prima="P2";
+		}else if(e.getMesesTrabajo()<12&& e.getDirectivo()=='+') {
+			this.prima="P3";
+		}else {
+			this.prima="P4";
+		}
+		this.numEmpleado= e.getNumEmpleado();
+	}
+
+	@Override
+	public String toString() {
+		return "Registro [nRegistro=" + nRegistro + ", prima=" + prima + ", numEmpleado=" + numEmpleado + "]";
+	}
+	
 	
 }
