b0b8c11492bbde0adfc5fbd138384c4941dbe5c4
casti-lob <casti_lob@hotmail.com>
2022-04-19 12:25:39 +0200

    mapas

--- Relacion11Ejercicio6/src/com/diccionario/Diccionario.java
+++ Relacion11Ejercicio6/src/com/diccionario/Diccionario.java
@@ -2,38 +2,55 @@ package com.diccionario;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 public class Diccionario {
-	 private static HashMap<Character, Palabra> abecedario = new HashMap<>();
+	private HashMap<String,HashSet<String> > palabras;
 	
-	public static void main(String[] args) {
-		inicializarAbecedario();
-		mostrarAbecedario();
-		ponerPalabra("amanecer");
-		ponerPalabra("casa");
-		ponerPalabra("amar");
-		mostrarAbecedario();
+	
+	
+	public Diccionario() {
+		super();
+		this.palabras = new HashMap<>();
+	}
+	
+	
+	
+	public  boolean annadirPalabra(String palabra, String significado) {
+		boolean resultado=true;
+		//Tengo que averiguar si la palabra ya esta en el diccionario
+		HashSet<String> valor = this.palabras.get(palabra);//esto consigue la lista de significados
+		
+		if(this.palabras.get(palabra)==null) {
+			HashSet<String> significados = new HashSet<>();
+			significados.add(significado);
+			if(this.palabras.put(palabra, significados)==null) {
+				resultado=false;
+			}
+		}else {//Si la palabra ya esta en el diccionario
+			resultado = valor.add(significado); //metemos la lista de significado y si son repetidos pues nos da un false
+		}
+		
+		return resultado;
+		
+	}
+	public boolean borrarPalabra(String palabra) {
+		HashSet<String> valor = this.palabras.get(palabra);
+		return palabras.remove(palabra,valor);
+	}
+	
+	public boolean borrarSignificadoPalabra(String palabra, String significado) {
+		HashSet<String> valor = this.palabras.get(palabra);
+		return valor.remove(significado);
 	}
 
-	
-	public static void inicializarAbecedario() {
-		
-		for(int i=65;i<91;i++) {
-			Character obtenerLetra = (char)i;
-			abecedario.put(obtenerLetra, null);
-		}
+
+
+	@Override
+	public String toString() {
+		return "Diccionario [palabras=" + palabras + "]";
 	}
 	
-	public static void mostrarAbecedario() {
-		System.out.println(abecedario);
-		
-	}
-	
-	public static void ponerPalabra(String palabra) {
-		palabra= palabra.toUpperCase();
-		Character inicial =palabra.charAt(0); 
-		abecedario.put(inicial, new  Palabra(palabra));
-	}
 }
--- Relacion11Ejercicio6/src/com/diccionario/Menu.java
+++ Relacion11Ejercicio6/src/com/diccionario/Menu.java
@@ -3,7 +3,14 @@ package com.diccionario;
 public class Menu {
 
 	public static void main(String[] args) {
+		Diccionario d= new Diccionario();
+		d.annadirPalabra("casa", "hogar");
+		d.annadirPalabra("casa", "edificio");
+		System.out.println(d);
+		System.out.println(d.borrarSignificadoPalabra("casa", null));
+		System.out.println(d.borrarPalabra("casa"));
 		
+		System.out.println(d);
 			
 	}
 
